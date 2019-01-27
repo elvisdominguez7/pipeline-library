ffe1d1dd556573b6b27287f0061cdf15b88e75f4
elvisdominguez7 <elvisdominguez7@users.noreply.github.com>
2019-01-27 11:54:11 -0600

    New Pcf Commands

--- src/org/edominguez/utils/PcfUtils.groovy
+++ src/org/edominguez/utils/PcfUtils.groovy
@@ -1,1 +1,19 @@
-test test
+package src.org.edominguez.utils;
+
+class PcfUtils{
+  static domain;
+  static org;
+  static space;
+  
+  PcfUtils (domain,org,env){
+    this.domain = domain;
+    this.org = org;
+    this.space = env;
+  }
+  
+  static login(){
+    
+    
+  }
+  
+}
